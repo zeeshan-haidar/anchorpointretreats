dec097902e7cf2b68371a39f18bcdb4f2b1f9b9b
zeeshan-haidar <zeshan.webdeveloper@gmail.com>
2026-05-16 00:24:23 +0500

    images and fonts issue resolved

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'webfo
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( *.woff2 *.woff )
+Rails.application.config.assets.precompile += %w( *.woff2 *.woff Lora/Lora-Regular.woff2 Lora/Lora-Regular.woff InterTight/InterTight-Regular.woff2 InterTight/InterTight-Regular.woff fa-solid-900.woff2 fa-brands-400.woff2 fa-regular-400.woff2 fa-v4compatibility.woff2 )
